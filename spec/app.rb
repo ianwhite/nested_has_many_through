2791b5dce34435ed32fd34bef06d30a8b74c817f
Ian White <ian.w.white@gmail.com>
2008-04-28 17:45:38 +0100

    Added some failing specs for newly created author case

--- spec/app.rb
+++ spec/app.rb
@@ -6,19 +6,20 @@
 
 ActiveRecord::Migration.suppress_messages do
   ActiveRecord::Schema.define(:version => 0) do
-    create_table :authors, :force => true do |t|
+    create_table :users, :force => true do |t|
+      t.column "type", :string
     end
     
-    create_table :post, :force => true do |t|
+    create_table :posts, :force => true do |t|
       t.column "author_id", :integer
       t.column "category_id", :integer
     end
 
-    create_table :category, :force => true do |t|
+    create_table :categories, :force => true do |t|
     end
 
-    create_table :comment, :force => true do |t|
-      t.column "author_id", :integer
+    create_table :comments, :force => true do |t|
+      t.column "user_id", :integer
       t.column "post_id", :integer
     end
   end
@@ -26,15 +27,24 @@ end
 
 #########
 # Models
-#########
+#
+# Domain model is this:
+#
+#   - authors (type of user) can create posts in categories
+#   - users can comment on posts
+#   - authors have recommended_posts: posts in the same categories as ther posts
+#   - authors have recommended_authors: authors of the recommended_posts
+#   - authors have commenters: users who have commented on their posts
+#
+class User < ActiveRecord::Base
+  has_many :comments
+end
 
-class Author < ActiveRecord::Base
+class Author < User
   has_many :posts
   has_many :categories, :through => :posts
   has_many :recommended_posts, :through => :categories, :source => :posts
   has_many :recommended_authors, :through => :recommended_posts, :source => :author
-  has_many :posts_of_recommended_authors, :through => :recommended_authors, :source => :posts
-
   has_many :commenters, :through => :posts
 end
 
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,11 +2,27 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../app'))
 
 describe Author do
-  describe "create" do
+  describe "newly created" do
     before { @author = Author.create }
   
-    it "should be cool" do
-      @author.should be_instance_of(ActiveRecord::Base)
+    it ".posts should == []" do
+      @author.posts.should == []
+    end
+     
+    it ".categories should == []" do
+      @author.categories.should == []
+    end
+    
+    it ".recommended_posts should == []" do
+      @author.recommended_posts.should == []
+    end
+    
+    it ".recommended_authors should == []" do
+      @author.recommended_authors.should == []
+    end
+    
+    it ".commenters should == []" do
+      @author.commenters.should == []
     end
   end
 end
