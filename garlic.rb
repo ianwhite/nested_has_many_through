45500a3d4e9b7f2d60eba3ee20992a2863d829cd
Ian White <ian.w.white@gmail.com>
2008-11-25 17:49:54 +1100

    Fixing some typos for cruise/doc/garlic

--- garlic.rb
+++ garlic.rb
@@ -6,23 +6,22 @@ garlic do
   repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'
   
   # target rails versions
-  ['origin/2-2-stable', 'origin/2-1-stable', 'origin/2-0-stable'].each do |rails|    
-      # specify how to prepare app and run CI task
-      target "Rails: #{rails}", :tree_ish => rails do
-        prepare do
-          plugin 'rspec'
-          plugin 'rspec-rails' do
-            `script/generate rspec -f`
-          end
-          plugin 'nested_has_many_through', :clone => true
+  ['origin/2-2-stable', 'origin/2-1-stable', 'origin/2-0-stable'].each do |rails|
+    # specify how to prepare app and run CI task
+    target "Rails: #{rails}", :tree_ish => rails do
+      prepare do
+        plugin 'rspec'
+        plugin 'rspec-rails' do
+          `script/generate rspec -f`
         end
-  
-        run do
-          cd "vendor/plugins/nested_has_many_through" do
-            sh "rake spec:rcov:verify"
-          end
+        plugin 'nested_has_many_through', :clone => true
+      end
+
+      run do
+        cd "vendor/plugins/nested_has_many_through" do
+          sh "rake spec:rcov:verify"
         end
       end
-    end # rspecs
-  end # railses
+    end
+  end
 end
